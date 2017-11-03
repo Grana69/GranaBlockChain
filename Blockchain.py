65a0536141418b61951d5e5f708605cf05ae9abb
Unknown <vgrana@uabc.edu.mx>
2017-11-03 02:36:48 -0800

    Cadena de bloques
    Se agrego una cadena de bloques
    Se implemento sha256
    Se agrego un inicio de la cadena

--- Blockchain.py
+++ Blockchain.py
@@ -1,23 +1,47 @@
+import hashlib
 class Block:
 	def __init__(self,ph,ts):
 		self.ph = ph #Hash anterior
-		self.ts = ts #Transaccion
-		self.cn = str([hash(ts),ph]) #Combinacion hash datos y Hash anterior
-		self.bh = hash(self.cn) #Generacion del nuevo hash
+		self.ts = ts #Transaccion #aqui se pueden agregar los datos necesarios
+		self.bh = "" #Hash a calcular
+	def __str__(self):
+		return self.bh
+	def Calcular(self):
+		self.bh = self.cSha256(str([self.cSha256(self.ts),self.ph]))
 	def getPh(self):
 		return self.ph
 	def getTs(self):
 		return self.ts
 	def getBh(self):
 		return self.bh
+	def cHash(self,cadena):
+		return hash(cadena);
+	def cSha256(self,cadena):
+		return hashlib.sha256(cadena).hexdigest()
 
-ts = "Enviar 10 objetos"
-b1 = Block(0,ts)
 
-ts2 = "Enviar 13 Objetos"
-b2 = Block(b1.getBh(),ts2)
+class CadenaDeBloques:
+	def __init__(self):
+		self.cadena = [self.primerBloque()]
+	def primerBloque(self):
+		bl = Block(0,"Primer Bloque")
+		bl.Calcular()
+		return bl
+	def getUltimoBloque(self):
+		return self.cadena[len(self.cadena) -1 ]
+	def setNuevoBloque(self,bloque):
+		bloque.ph = self.getUltimoBloque().bh
+		bloque.Calcular()
+		self.cadena.append(bloque)
+	def imprimirCadena(self):
+		print "\nCadena de bloques:\n"
+		for item in self.cadena:
+			print "Transaccion: %s" % item.ts
+			print "Hash: %s" % item.bh
+			print "Hash Anterior: %s\n \n " % item.ph
 
-print "Hash Bloque 1: "
-print b1.getBh()
-print "Hash Bloque 2: " 
-print b2.getBh()
+
+bs = CadenaDeBloques()
+bs.setNuevoBloque(Block(0,"Enviar 11 objetos"))
+bs.setNuevoBloque(Block(1,"Enviar 13 objetos"))
+bs.imprimirCadena()
